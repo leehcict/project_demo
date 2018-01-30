51e9065e76901bf8e6c5f2940d2dae1904f46309
leehcict <leehcict@gmail.com>
2018-01-30 14:05:55 +0700

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return this.CustomerName+"|"+this.Address;
+        }
+    }
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World is old now. It’s time for wassup bro ;)";
+        }
+
+        [NonAction]
+        public string SimpleMethod()
+        {
+            return "Hi, I am not action method";
+        }
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Customer 1";
+            c.Address = "Address1";
+            return c;
+        }
+
+public ActionResult GetView()
+{
+    if(Some_Condition_Is_Matching)
+    { 
+        return View("MyView");
+    }
+    else
+    {
+        return Content("Hi Welcome");
+    }
+}
+    }
+}
